9640da489ae2c282185c8d296ce3970c76f6805c
wingmeng <wingmeng@qq.com>
2024-06-30 17:42:13 +0800

    :fire: 清理 examples

--- examples/vue2-elementui/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  devServer: {
-    port: 5262
-  },
-  transpileDependencies: true
-})
--- examples/vue3-elementplus/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
-import 'element-plus/dist/index.css'
-import MergeTableCells from '../../../dist/merge-table-cells.js'
-
-createApp(App)
-  .provide('MergeTableCells', MergeTableCells)
-  .mount('#app')
